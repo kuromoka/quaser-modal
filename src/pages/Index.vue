fb24823db81f4730fbdb22d744127fe8b1410b4f
kuromoka <kuromokapb@gmail.com>
2019-12-25 19:01:37 +0900

    fixed index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="flex flex-center column">
+    <q-btn label="Modal" color="primary" @click="modal = true" />
+
+    <q-dialog v-model="modal">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Modal</div>
+        </q-card-section>
+
+        <q-card-section>
+          <q-input v-model="text" label="Text" />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <p>{{ text }}</p>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      modal: false,
+      text: ''
+    }
+  }
 }
 </script>
